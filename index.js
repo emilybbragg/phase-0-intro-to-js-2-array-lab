0e8045cb3bac4177dce972387108563b8e32d58d
Emily <emilybribragg@gmail.com>
2021-11-23 13:05:15 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    console.log("Name:", name)
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    console.log("Name:", name)
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+//NOTE: need RETURN when creating new array
+function appendCat(name) {
+   return  [...cats, name]
+}
+
+function prependCat(name) {
+    return  [name, ...cats]
+ }
+
+ function removeLastCat(name) {
+  return cats.slice(0, cats.length-1);
+ }
+
+ function removeFirstCat(name) {
+    return cats.slice(1);
+   }
